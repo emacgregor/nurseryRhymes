d24f575714c70636cee740f0051c58338b9356a4
Guest <guest@Jacks-MacBook-Pro.local>
2000-12-31 19:24:25 -0500

    Created iOS project

---
+++ nursery-rhymes/nursery-rhymesTests/nursery_rhymesTests.swift
@@ -0,0 +1,36 @@
+//
+//  nursery_rhymesTests.swift
+//  nursery-rhymesTests
+//
+//  Created by Guest on 12/31/00.
+//  Copyright © 2000 Team8343. All rights reserved.
+//
+
+import XCTest
+@testable import nursery_rhymes
+
+class nursery_rhymesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
