7c97af23ac0beb66a1365f9def09335f2a6e6fcd
Guest <guest@Jacks-MacBook-Pro.local>
2000-12-31 21:46:23 -0500

    Expanding storyboard

---
+++ nursery-rhymes/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  nursery-rhymes
+//
+//  Created by Guest on 12/31/00.
+//  Copyright © 2000 Team8343. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    var m = Model()
+    @IBOutlet weak var rhymeText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Running")
+        
+        m = Model.getModel()
+        m.readFile(fileName: "V_21_WillieBoy")
+        
+        //print(m.collections[0][0])
+        rhymeText.text = m.getRhymeText(rawText: m.collections["Volland"]!["Willie Boy"]!)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- nursery-rhymes/nursery-rhymes/RhymeViewController.swift
+++ nursery-rhymes/nursery-rhymes/RhymeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RhymeViewController: UIViewController {
 
     var m = Model()
     @IBOutlet weak var rhymeText: UILabel!
