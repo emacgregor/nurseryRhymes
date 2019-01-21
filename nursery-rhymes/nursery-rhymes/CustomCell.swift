9d78522095adfbd94c9d63372d122a6c7132dd9a
Zachary Cheshire <zcheshire3@gatech.edu>
2019-01-20 22:24:28 -0500

    Added constraints for Cell

--- nursery-rhymes/nursery-rhymes/CustomCell.swift
+++ nursery-rhymes/nursery-rhymes/CustomCell.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import UIKit
+
+class CustomCell: UITableViewCell {
+    var message: String?
+    var mainImage: UIImage?
+    
+    var messageView : UITextView = {
+        var textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    var mainImageView : UIImageView = {
+        var imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.addSubview(mainImageView)
+        self.addSubview(messageView)
+        mainImageView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        mainImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        mainImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        mainImageView.widthAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+        
+        messageView.leftAnchor.constraint(equalTo: self.mainImageView.rightAnchor).isActive = true
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- nursery-rhymes/nursery-rhymes/RhymesTableController.swift
+++ nursery-rhymes/nursery-rhymes/RhymesTableController.swift
@@ -5,7 +5,7 @@
 //  Created by Zachary Cheshire on 1/20/19.
 //  Copyright © 2019 Team8343. All rights reserved.
 //
-
+import Foundation
 import UIKit
 struct cellData {
     let image: UIImage?
