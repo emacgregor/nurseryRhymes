6933333fd25427e31063ee1b050da85873a69ce2
Ethan MacGregor <ethanbrs1@gmail.com>
2019-02-13 22:01:36 -0500

    Mother Goose manually sorted

--- Rhyme_packs/parse.py
+++ Rhyme_packs/parse.py
@@ -1,24 +1,31 @@
 import json
+import os
 from pprint import pprint
+import shutil
 
-with open('rhymeText.json', encoding='utf8', errors = 'ignore') as data_file:
+"""for file in os.listdir('Mother Goose/Successes/'):
+    print ('Mother Goose/Successes/' + file[9:])
+    os.rename('Mother Goose/Successes/' + file, 'Mother Goose/Successes/' + file[9:])"""
+
+with open('rhymeText.json', 'r', encoding='utf8') as data_file:
     parsed_json = json.load(data_file)
 
 counter = 1
-for rhyme in parsed_json['rhymes']:
-    while (counter < 239):
-        counter += 1
-        if (counter % 10 == 1):
-            counter += 1
-        address = str(counter) + "r"
-        print (rhyme[address]['title'])
-        if (rhyme[address]["collection"] == "Volland"):
-            print ("Sorting into Volland")
-
-
-            name = 'Volland_Packs/V_' + str(rhyme[address]["id"]) + '/text.txt'
-            with open(name, "w") as g:
-                g.write(rhyme[address]["text"])
-            name = 'Volland_Packs/V_' + str(rhyme[address]["id"]) + '/title.txt'
-            with open(name, "w") as g:
-                g.write(rhyme[address]["title"])
+for folder in os.listdir('Mother Goose/Failures'):
+    title = ""
+    """if (len(os.listdir('Mother Goose/Failures/' + folder)) == 1):
+        shutil.rmtree("Mother Goose/Failures/" + folder)
+    else:"""
+    for file in os.listdir('Mother Goose/Failures/' + folder):
+        if (file[-5:] == "title"):
+            title = file[:-6]
+    print (title)
+    for rhyme in parsed_json:
+        if (rhyme["title"] == title and rhyme["collection"] == "Mother Goose Visit"):
+            print ("I found one, I found one!")
+            file = open("Mother Goose/Successes/" + title + "MGV.txt", "w")
+            file.write(rhyme["text"])
+            os.rename('Mother Goose/Failures/' + folder + "/Artwork.jpg", "Mother Goose/Successes/" + title + "MGV.jpg")
+            os.rename('Mother Goose/Failures/' + folder + "/Audio.mp3", "Mother Goose/Successes/" + title + "MGV.mp3")
+            os.rename('Mother Goose/Failures/' + folder + "/transcript.txt", "Mother Goose/Successes/" + title + "MGV.transcript")
+            shutil.rmtree("Mother Goose/Failures/" + folder)
