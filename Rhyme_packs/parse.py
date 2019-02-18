1c5dc01a05556b9820c95d802e9763e873674c88
Ethan MacGregor <ethanbrs1@gmail.com>
2019-02-18 13:50:46 -0500

    Sorted FGV

--- Rhyme_packs/parse.py
+++ Rhyme_packs/parse.py
@@ -3,29 +3,29 @@ import os
 from pprint import pprint
 import shutil
 
-"""for file in os.listdir('Mother Goose/Successes/'):
-    print ('Mother Goose/Successes/' + file[9:])
-    os.rename('Mother Goose/Successes/' + file, 'Mother Goose/Successes/' + file[9:])"""
+"""for file in os.listdir('Father Goose/Successes/'):
+    print ('Father Goose/Successes/' + file[9:])
+    os.rename('Father Goose/Successes/' + file, 'Father Goose/Successes/' + file[9:])"""
 
 with open('rhymeText.json', 'r', encoding='utf8') as data_file:
     parsed_json = json.load(data_file)
 
 counter = 1
-for folder in os.listdir('Mother Goose/Failures'):
+for folder in os.listdir('Father Goose/Failures'):
     title = ""
-    """if (len(os.listdir('Mother Goose/Failures/' + folder)) == 1):
-        shutil.rmtree("Mother Goose/Failures/" + folder)
+    """if (len(os.listdir('Father Goose/Failures/' + folder)) == 1):
+        shutil.rmtree("Father Goose/Failures/" + folder)
     else:"""
-    for file in os.listdir('Mother Goose/Failures/' + folder):
+    for file in os.listdir('Father Goose/Failures/' + folder):
         if (file[-5:] == "title"):
             title = file[:-6]
     print (title)
     for rhyme in parsed_json:
-        if (rhyme["title"] == title and rhyme["collection"] == "Mother Goose Visit"):
+        if (rhyme["title"] == title and rhyme["collection"] == "Father Goose Visit"):
             print ("I found one, I found one!")
-            file = open("Mother Goose/Successes/" + title + "MGV.txt", "w")
+            file = open("Father Goose/Successes/" + title + "FGV.txt", "w")
             file.write(rhyme["text"])
-            os.rename('Mother Goose/Failures/' + folder + "/Artwork.jpg", "Mother Goose/Successes/" + title + "MGV.jpg")
-            os.rename('Mother Goose/Failures/' + folder + "/Audio.mp3", "Mother Goose/Successes/" + title + "MGV.mp3")
-            os.rename('Mother Goose/Failures/' + folder + "/transcript.txt", "Mother Goose/Successes/" + title + "MGV.transcript")
-            shutil.rmtree("Mother Goose/Failures/" + folder)
+            os.rename('Father Goose/Failures/' + folder + "/Artwork.jpg", "Father Goose/Successes/" + title + "FGV.jpg")
+            os.rename('Father Goose/Failures/' + folder + "/Audio.mp3", "Father Goose/Successes/" + title + "FGV.mp3")
+            os.rename('Father Goose/Failures/' + folder + "/transcript.txt", "Father Goose/Successes/" + title + "FGV.transcript")
+            shutil.rmtree("Father Goose/Failures/" + folder)
