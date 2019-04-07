857b3f791d637bb7cd6e8690b547d8aa52037aba
Jack Bonaguro <jbonaguro6@gatech.edu>
2019-04-07 04:50:42 -0400

    Sexy buttons

--- nursery-rhymes/nursery-rhymes/RoundButton.swift
+++ nursery-rhymes/nursery-rhymes/RoundButton.swift
@@ -11,6 +11,34 @@ import UIKit
 @IBDesignable
 class RoundButton: UIButton {
     
+    private var shadowLayer: CAShapeLayer!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if shadowLayer == nil {
+            shadowLayer = CAShapeLayer()
+            shadowLayer.position = CGPoint(x: center.x - bounds.width / 2,
+                                           y: center.y - bounds.height / 2)
+            shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: 35).cgPath
+            shadowLayer.fillColor = UIColor.white.cgColor
+            
+            shadowLayer.shadowColor = UIColor.darkGray.cgColor
+            shadowLayer.shadowPath = shadowLayer.path
+            shadowLayer.shadowOffset = CGSize(width: 5.0, height: 5.0)
+            shadowLayer.shadowOpacity = 0.8
+            shadowLayer.shadowRadius = 5
+            shadowLayer.masksToBounds = true
+            shadowLayer.masksToBounds = false
+            
+            self.superview?.layer.insertSublayer(shadowLayer, at: 0)
+            //self.superview?.bringSubview(toFront: self)
+            
+            //layer.insertSublayer(shadowLayer, at: 2)
+            //layer.insertSublayer(shadowLayer, below: nil) // also works
+        }
+    }
+
     @IBInspectable var cornerRadius: CGFloat = 0{
         didSet{
             self.layer.cornerRadius = cornerRadius
@@ -29,5 +57,11 @@ class RoundButton: UIButton {
         }
     }
     
-   
+    @IBInspectable var shadowOffset: CGSize = CGSize(width: 10, height: 10) {
+        didSet{
+            self.layer.shadowOffset = shadowOffset
+            self.layer.shadowRadius = 5
+            self.layer.shadowOpacity = 0.5
+        }
+    }
 }
