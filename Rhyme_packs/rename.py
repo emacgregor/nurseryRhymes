f44158fca9e04f4a8a75c47c35ca6094925bf1f4
Ethan MacGregor <ethanbrs1@gmail.com>
2019-04-08 17:59:19 -0400

    Added relevant files and removed ones that are no longer necessary

--- Rhyme_packs/rename.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for file in os.listdir('Jerrold Rhymes/'):
-    print (file)
-    name = 'Jerrold Rhymes/' + file
-    print (name)
-    name2 = name
-    print (name2)
-    os.rename(name, name2.split('.')[0] + 'Jerrold.' + name2.split('.')[1])
